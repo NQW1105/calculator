439e924893bc99f8f496409d0640f6ab8508aa87
NQW1105 <neoqw@hotmail.com>
2022-10-01 15:56:25 +0800

    Complete JS code

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function divide(num1, num2) {
 
 function operate(operator, num1, num2) {
   let result = operator(num1, num2);
-  // console.log(result);
   return result;
 }
 
 let num = "";
 let numStorage = [];
 let choosenOperator = "";
+let calculated = "";
 
 let mainDisplay = document.querySelector('.main-display');
 let miniDisplay = document.querySelector('.mini-display');
@@ -36,17 +36,127 @@ numButtons.forEach(button => button.addEventListener('click', addNum));
 
 
 function selectOperator(e) {
-  choosenOperator = e.target.innerText;
-  if (num == "") {
+  // console.log(e);
+  
+  // Button does nothing when there's no number yet
+  if (num == "" && calculated == "") {
     return;
+  } 
+
+  if (num != "") {
+    numStorage.push(parseInt(num));
   }
-  numStorage.push(num);
-  miniDisplay.textContent = `${num} ${choosenOperator}`;
-  num = "";
+  
+
+  // console.log(numStorage);
+  
+
+  if (calculated == "") {
+
+    if (numStorage.length >= 2) {
+      if (choosenOperator == "add") {
+        calculated = operate(add, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "substract") {
+        calculated = operate(subtract, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "multiply") {
+        calculated = operate(multiply, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "divide") {
+        calculated = operate(divide, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+      }
+      choosenOperator = e.target.classList[1];
+      miniDisplay.textContent = `${calculated} ${e.target.textContent}`;
+      num = "0";
+      mainDisplay.textContent = num;
+    } else {
+      choosenOperator = e.target.classList[1];
+      miniDisplay.textContent = `${numStorage[0]} ${e.target.textContent}`;
+      num = "0";
+      mainDisplay.textContent = num;
+      return;
+    }
+  } else {
+    
+    if (num != "") {
+      if (choosenOperator == "add") {
+        calculated = operate(add, calculated, numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "substract") {
+        calculated = operate(subtract, calculated, numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "multiply") {
+        calculated = operate(multiply, calculated, numStorage[numStorage.length - 1]);
+      } else if (choosenOperator == "divide") {
+        calculated = operate(divide, calculated, numStorage[numStorage.length - 1]);
+      }
+      num = "0";
+      choosenOperator = "";
+      mainDisplay.textContent = num;
+    }
+    choosenOperator = e.target.classList[1];
+    miniDisplay.textContent = `${calculated} ${e.target.textContent}`;
+  }
+
+  // console.log(calculated);
 }
 let operatorButtons = document.querySelectorAll('.ops');
 operatorButtons.forEach(button => button.addEventListener('click', selectOperator));
 
-// let equalButton = document.querySelector('.equal');
-// equalButton.addEventListener('click', operate);
 
+function calculate(e) {
+  if (num == "" || choosenOperator == "") {
+    return;
+  } 
+
+  numStorage.push(parseInt(num));
+  if (calculated == "") {
+    if (choosenOperator == "add") {
+      calculated = operate(add, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "substract") {
+      calculated = operate(subtract, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "multiply") {
+      calculated = operate(multiply, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "divide") {
+      calculated = operate(divide, numStorage[numStorage.length - 2], numStorage[numStorage.length - 1]);
+    }
+    miniDisplay.textContent = `${calculated}`;
+    num = "0";
+    choosenOperator = "";
+    mainDisplay.textContent = num;
+  } else {
+    if (choosenOperator == "add") {
+      calculated = operate(add, calculated, numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "substract") {
+      calculated = operate(subtract, calculated, numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "multiply") {
+      calculated = operate(multiply, calculated, numStorage[numStorage.length - 1]);
+    } else if (choosenOperator == "divide") {
+      calculated = operate(divide, calculated, numStorage[numStorage.length - 1]);
+    }
+    num = "0";
+    choosenOperator = "";
+    mainDisplay.textContent = num;
+    miniDisplay.textContent = `${calculated}`;
+  }
+}
+let equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', calculate);
+
+function clearAll() {
+  numStorage = [];
+  num = "0";
+  calculated = "";
+  choosenOperator = '';
+  miniDisplay.textContent = "";
+  mainDisplay.textContent = `${num}`;
+}
+let clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearAll);
+
+function deleteNum() {
+  num = num.slice(0, -1);
+  mainDisplay.textContent = `${num}`;
+}
+let delButton = document.querySelector('.del');
+delButton.addEventListener('click', deleteNum);
+
+// Floating point numbers (3/4 decimal points)
+// Show "error" when num divided by 0
+// Keyboard support??? Leave this last...
